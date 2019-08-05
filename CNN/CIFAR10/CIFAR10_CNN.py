eacb6b010ac99efdd92593c464871612659b787c
Karol Oleszek <oleszek.karol@gmail.com>
2019-08-05 14:47:31 +0200

    CIFAR10 AUGMENTED CNN

---
+++ CNN/CIFAR10/CIFAR10_CNN.py
@@ -0,0 +1,59 @@
+# coding=utf-8
+"""CIFAR 10 Dataset classifier using convolutional neural network (CNN)."""
+
+from keras.datasets import cifar10
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import SGD, Adam, RMSprop
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    IMG_CHANNELS = 3
+    IMG_X_SIZE = 32
+    IMG_Y_SIZE = 32
+
+    BATCH_SIZE = 128
+    EPOCHS = 20
+    CLASSES = 10
+    VERBOSE = 2
+    VALIDATION_SPLIT = 0.2
+    OPTIMIZER = RMSprop()
+
+    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
+    print("X_train shape:", X_train.shape)
+    print(X_train.shape[0], "train samples")
+    print(X_test.shape[0], "test samples")
+
+    Y_train = np_utils.to_categorical(y_train, CLASSES)
+    Y_test = np_utils.to_categorical(y_test, CLASSES)
+
+    X_train = X_train.astype("float32")
+    X_test = X_test.astype("float32")
+    X_train /= 255
+    X_test /= 255
+
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding="same", input_shape=(IMG_X_SIZE, IMG_Y_SIZE, IMG_CHANNELS)))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(200))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(CLASSES))
+    model.add(Activation('softmax'))
+    model.summary()
+
+    model.compile(loss="categorical_crossentropy", optimizer=OPTIMIZER, metrics=["accuracy"])
+    model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=VALIDATION_SPLIT, verbose=VERBOSE)
+    score = model.evaluate(X_test, Y_test, batch_size=BATCH_SIZE, verbose=VERBOSE)
+    print("Test score:", score[0])
+    print("Test accuracy:", score[1])
+
+    model_json = model.to_json()
+    open("cifar10_cnn_model.json", "w").write(model_json)
+    model.save_weights("cifar10_cnn_weights.h5", overwrite=True)
